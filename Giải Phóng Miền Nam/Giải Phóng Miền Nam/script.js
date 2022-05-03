193dbd7770a3b7cd9218616e903c3181aaae8328
ManhNguyen452 <104673720+ManhNguyen452@users.noreply.github.com>
2022-05-03 21:27:45 +0700

    update

--- Giải Phóng Miền Nam/Giải Phóng Miền Nam/script.js
+++
@@ -1,7 +0,0 @@
-﻿var h = $('.flag').width();
-for (var i = 0; i < h; i++) {
-    var flagElement = $("<div class='flag-element'>");
-    flagElement.css('animation-delay', i * 10 + 'ms');
-    flagElement.css('background-position', -i + "px 0");
-    $('.flag').append(flagElement);
-}
